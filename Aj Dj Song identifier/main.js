8cbf66a3d647fb125f6e5e667a41b7dc116edd76
ChocolateCupcake25 <92889263+ChocolateCupcake25@users.noreply.github.com>
2022-01-03 23:53:33 +0100

    Add files via upload

--- Aj Dj Song identifier/main.js
+++ Aj Dj Song identifier/main.js
@@ -1,7 +1,15 @@
+song_1='';
+song_2='';
 function preload(){
-
+song_1=loadSound('harry_potter.mp3');
+song_2=loadSound('peter_pan.mp3');
 }
 function setup(){
-    video=createCapture(500,485);
-    video.position(385,150);
+    canvas=createCanvas(500,485);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,500,485);
 }
